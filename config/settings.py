72bbd12c271082e8080e00cec774b541ae6c5128
Franklin Ramos <ramosfp99@gmail.com>
2026-06-12 02:13:16 +0800

    updates cors

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,14 @@ CORS_ALLOWED_ORIGINS = [
     'https://eld.franklinramos.site',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://eld.franklinramos.site",
+]
+
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
