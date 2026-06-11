92e2b096c5b89362b4722a273849e557d10e7bd9
Franklin Ramos <ramosfp99@gmail.com>
2026-06-12 02:02:13 +0800

    updates

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://eld.franklinramos.site',
 ]
 
 # REST Framework
